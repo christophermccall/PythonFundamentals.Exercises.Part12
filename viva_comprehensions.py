83a1d276f1254fb6b5cc49d9af322af9f611fe85
christophermccall <camccall24@gmail.com>
2024-03-19 23:06:15 -0400

    finished. fixed each funcion and added notes to combat the confusion brought on by the evil/misunderstood developer

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -18,7 +18,20 @@ def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     :param parity:
     :return:
     """
-    pass
+    # because parity.EVEN is set to an odd number and parity.ODD is set to an even number...
+    # ...this list takes in x IF it's even or odd based on the OPPOSITE category(odd,even) of the parity value
+    list_gen = [x for x in range(start, stop) if x % 2 != parity.value]
+    return list_gen
+
+    # another example
+
+    # if parity == parity.ODD: -if parity value is even...
+    #     list_gen = [x for x in range(start, stop) if x % 2 == 0]# ...this list takes even numbers...
+    #     ...from the specified start because enum ODD is set to an even number.
+    # else:...
+    #     list_gen = [x for x in range(start, stop) if x % 2 != 0]# ...this list takes in odd numbers...
+    #     ....from the specified start because enum EVEN is set to an odd number
+    # return list gen
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
@@ -33,8 +46,14 @@ def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     :param strategy:
     :return:
     """
-    pass
+    # this function takes in a key and uses a predetermined "strategy()"...
+    # ...such as squaring a number or finding the factorial...
+    # ...and manipulates the key via strategy(key) in order to determine and return the {key: value}
+    # key == {key:      }
+    #        {   : value}  == strategy(key)
 
+    dict_gen = {key: strategy(key) for key in range(start, stop)}
+    return dict_gen
 
 def gen_set(val_in: str) -> Set:
     """
@@ -45,4 +64,8 @@ def gen_set(val_in: str) -> Set:
     :param val_in:
     :return:
     """
-    pass
+    # this set takes only the lower case characters from a string(character list) and turns them into upper case...
+    # ...while inherently eliminating doubles
+    set_gen = {val.upper() for val in val_in if val.islower()}
+
+    return set_gen
